b68012576442a698ba1d7c5ff9c11bd2e59f1ec4
ador23-protiva <protiva.ador07@gmail.com>
2026-09-11 09:39:01 +0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Nav from './components/nav'
+
+function App() {
+  return (
+    <>
+        <h2 className='text-4xl text-amber-300'>B14-A05</h2>
+        <Nav/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Nav.tsx
@@ -0,0 +1,52 @@
+import Logo from '../assets/logo-text.png'
+
+const Nav = () => {
+    return (
+         <nav className="border-b border-gray-100 bg-white">
+      <div className="mx-auto flex h-14 max-w-6xl items-center justify-between px-6">
+
+        {/* Logo */}
+        <div className="flex items-center gap-2">
+          <img src={Logo} alt="" />
+        </div>
+
+        {/* Menu */}
+        <div className="hidden items-center gap-7 md:flex">
+          <a href="#" className="text-sm font-medium text-pink-500">
+            Home
+          </a>
+
+          <a href="#" className="text-sm text-gray-500 hover:text-pink-500">
+            Technologies
+          </a>
+
+          <a href="#" className="text-sm text-gray-500 hover:text-pink-500">
+            Projects
+          </a>
+
+          <a href="#" className="text-sm text-gray-500 hover:text-pink-500">
+            About
+          </a>
+
+          <a href="#" className="text-sm text-gray-500 hover:text-pink-500">
+            Contact
+          </a>
+        </div>
+
+        {/* Right Side */}
+        <div className="flex items-center gap-5">
+          <button className="text-sm text-gray-600 hover:text-pink-500">
+            Sign In
+          </button>
+
+          <button className="rounded-full bg-pink-500 px-5 py-2 text-sm font-medium text-white hover:bg-pink-600">
+            Sign Up
+          </button>
+        </div>
+
+      </div>
+    </nav>
+    );
+};
+
+export default Nav;
